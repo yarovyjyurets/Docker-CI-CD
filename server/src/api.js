194a512341a3d6102540cd77986222b6d76f4c14
Yurets <yurii_yarovyi@epam.com>
2019-12-20 12:00:52 +0200

    server: change text response

--- server/src/api.js
+++ server/src/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-  res.send('Simple node-app server ever!');
+  res.send('Simple node-app server ever (changed)!');
 });
 
 
